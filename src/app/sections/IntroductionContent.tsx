58952699ffbfa788d52b6b62f5086c343db9c9b6
thomas <thomas@serenity>
2025-07-23 14:17:30 +0200

    Modify labels

--- src/app/sections/IntroductionContent.tsx
+++ src/app/sections/IntroductionContent.tsx
@@ -1,8 +1,8 @@
 export default function IntroductionContent() {
   return (
   <div className="m-auto line-height-3 text-white max-w-30rem text-justify">
-    <p><strong>Passionate about cybersecurity and IT in general</strong>, I earned my DUT in Computer Science in 2022 and am on track to graduate from École 2600 in August 2025. <strong>Driven by creativity</strong>, I’m eager to join a motivated team where I can contribute to ambitious projects centered around software development and cybersecurity challenges, starting September/October 2025.</p>
-    <p>After several years of training and experience in both software development and cybersecurity, focusing on web development felt like a natural next step in my versatile journey. With this dual expertise, I’m excited to actively contribute to projects at the crossroads of web development and security — though I remain open to opportunities specifically in cybersecurity as well. Aware there’s still much to learn as a web developer, I humbly share my skills and aspirations across this personal website.</p>
+    <p><strong>Passionate about cybersecurity and IT in general</strong>, I earned my DUT in Computer Science in 2022 and am on track to graduate from École 2600 in August 2025. Driven by creativity, I’m eager to join a motivated team where I can contribute to ambitious projects centered around software development and cybersecurity challenges, starting September/October 2025.</p>
+    <p><strong>With a solid background in both software development and cybersecurity</strong>, I’m equally open to roles as a developer or a cybersecurity specialist. I’m especially drawn to projects where these two fields meet — but I also value opportunities that allow me to go deeper into one or the other. While I still have much to learn as a developer, I humbly share my skills, projects, and aspirations through this personal website.</p>
   </div>
   );
 };
